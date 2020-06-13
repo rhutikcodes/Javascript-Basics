05d4eb1c417fd59350363352ae237af7110864ea
Rhutik Giradkar <rhutikgiradkar@gmail.com>
2020-06-13 17:48:45 +0530

    Array and Template Literal

---
+++ 02Basics/arraysAndTemplateLiteral.js
@@ -0,0 +1,10 @@
+const superHeroes=['Iron Man', 'Thor', 'Capt. America', 'Spiderman']
+
+console.log(superHeroes[0]);
+
+console.log(superHeroes[2]);
+
+
+console.log(superHeroes[superHeroes.length -1]);
+
+console.log(`We have ${superHeroes.length} superheroes` );
