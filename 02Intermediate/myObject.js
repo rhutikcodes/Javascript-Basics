c015020b0c3d815596e0df68a6a90eb46382bf02
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2020-08-23 12:38:02 +0530

    Function that takes object and gives object

--- 02Intermediate/myObject.js
+++ 02Intermediate/myObject.js
@@ -5,6 +5,21 @@ discription:'A very short discription',
 videoCreator:'Rhutik'
 }
 
-console.log(myVideo)
+// console.log(myVideo)
 
-console.log(myVideo.title)
+// console.log(myVideo.title)
+
+
+let changeVideoLength=function(myObject){
+
+    return {
+        formatOne:`Time of this video is ${myObject.duration}`,
+        formatTwo:`Time of this video is ${myObject.duration +2}`
+    }
+
+}
+
+let adOne=changeVideoLength(myVideo)
+console.log(adOne);
+console.log(adOne.formatOne);
+console.log(adOne.formatTwo);
