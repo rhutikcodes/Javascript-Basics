79e68698aabcf5c5dceae7915fe6f75073a44b05
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2020-10-09 09:44:39 +0530

    express.js routes

--- NodeJS/03ExpressBasic/index.js
+++ NodeJS/03ExpressBasic/index.js
@@ -9,4 +9,33 @@ app.get('/about', function (req, res) {
   res.send('<h1>Hello there thanks for visiting</h1>')
 })
 
+app.post('/login', (req, res) => {
+  res.send('login success')
+})
+app.delete('/user', (req, res) => {
+  res.send('delete success')
+})
+
+//ids
+app.get('/user:id/status/:STATUS_ID', (req, res) => {
+  res.send(req.params)
+})
+
+app.get('/user:id', (req, res) => {
+  res.send(req.params)
+})
+app.get('/flights/:from-:to', (req, res) => {
+  res.send(req.params)
+})
+
+//regular expression
+
+//ab*cd - any string that starts with ab and ends with cd
+app.get('/ab*bc', (req, res) => {
+  res.send('regular expression')
+})
+app.get('/**', (req, res) => {
+  res.send('regular expression')
+})
+
 app.listen(3000, () => console.log('server is running at port 3000...'))
