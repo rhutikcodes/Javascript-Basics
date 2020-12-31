d8f25820a55a382d52e65fc149516411148d3f67
rhutikcodes <rhutikgiradkar@gmail.com>
2020-12-31 11:43:00 +0530

    i am back

--- NodeJS/03ExpressBasic/index.js
+++ NodeJS/03ExpressBasic/index.js
@@ -9,4 +9,35 @@ app.get('/about', function (req, res) {
   res.send('<h1>Hello there thanks for visiting</h1>')
 })
 
+
+app.get('/ab*cd', function (req, res) {
+  res.send('<h1>This is regex</h1>')  //any page starting with ab and ending with cd
+})
+
+app.get('/about-us', function (req, res) {
+  res.json({
+    user:'Rhutik',
+    balance:5000,
+    id:3249
+  })
+})
+
+
+app.get('/user/:id/status/:status_id', function (req, res) {
+  res.send(req.params)
+})
+
+app.post('/login',(req,res)=>{
+  res.send('Login success')
+})
+
 app.listen(3000, () => console.log('server is running at port 3000...'))
+
+
+// res.download()
+// res.end()
+// res.json()
+// res.send()
+// res.render()
+// res.redirect()
+// res.status()
