2a5f5d7774652267b13b70d90d2545d47a2ddd3c
rhutikcodes <rhutikgiradkar@gmail.com>
2021-01-02 19:06:52 +0530

    todo app finished

--- NodeJS/todolist/app.js
+++ NodeJS/todolist/app.js
@@ -1,12 +1,52 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const ejs = require('ejs');
+const date = require(__dirname + '/date.js');
+const app = express()
 
-const app=express()
+app.set('view engine', 'ejs')
 
-app.get('/',function (req,res) {
-    res.send('Hello')
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.use(express.static('public'))
+
+const items = ["Buy Food", "Cook Food", "Eat Food"]
+const workItems = []
+
+
+app.get('/', function (req, res) {
+    let day = date.getDate()
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    })
 })
 
-app.listen(3000,function () {
+app.post('/', function (req, res) {
+
+    const item = req.body.newItem
+
+    if (req.body.button === 'Work') {
+        workItems.push(item)
+        res.redirect('/work')
+    } else {
+        items.push(item)
+        res.redirect('/')
+    }
+
+
+})
+
+app.get('/work', function (req, res) {
+
+    res.render("list", {
+        listTitle: "Work",
+        newListItems: workItems
+    })
+})
+
+
+app.listen(3000, function () {
     console.log('server is live');
 })
+
---
+++ NodeJS/todolist/date.js
@@ -0,0 +1,7 @@
+
+exports.getDate = function getDate() {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const today = new Date();
+    return today.toLocaleDateString("en-us", options)
+}
+
